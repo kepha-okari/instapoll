451e60d0582189bdd191e0e4c6b638540770e1f2
rkepha <kephaokari@gmail.com>
2018-02-14 08:18:50 +0300

    added the models for group,follow,choice and profile

--- pollapp/models.py
+++ pollapp/models.py
@@ -1,14 +1,42 @@
 from django.db import models
-
+import datetime as dt
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
+    question_title = models.CharField(max_length=200)
+    date_started = models.DateTimeField(auto_now_add=True, null=True)
+    date_closing = models.DateTimeField(auto_now_add=True,null=True)
+    vote_count = models.IntegerField(default=0)
+
+
+
+class Group (models.Model):
+    photo = models.ImageField(upload_to = 'groups/', null=True,blank=True)
+    name = models.CharField(max_length=20)
+    user = models.ForeignKey(User)
+
 
 class Choice(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+    users = models.ForeignKey(User,null =True,blank=True,on_delete=models.CASCADE)
+    question = models.ForeignKey(Question,null =True,blank=True, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group,null =True,blank=True, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    photo = models.ImageField(upload_to = 'profiles/', null=True,blank=True)
+    email = models.EmailField(max_length=70,blank=True)
+    phone = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Follow(models.Model):
+    '''
+    Class that store a User and Profile follow group precedings
+    '''
+    user = models.ForeignKey(User,null=True,blank=True  )
+    group = models.ForeignKey(Group,null=True,blank=True)
